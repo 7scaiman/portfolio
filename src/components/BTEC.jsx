216174f35b6a06e418efec695b1b3c77bb55753e
Oleksandr Hanysh <sanya.hanysh@gmail.com>
2026-05-12 11:22:36 +0100

    BTEC 4

--- src/components/BTEC.jsx
+++ src/components/BTEC.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import {Box, Grid} from "@mui/material";
-import  './CSS/BTEC.css';
-import img from "../../public/city-of-liverpool-college-black-logo-480x180.png"
-import CodeIcon from '@mui/icons-material/Code';
-import SchoolIcon from '@mui/icons-material/School';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import StorageIcon from '@mui/icons-material/Storage';
-function BTEC(props) {
-    return (
-        <section id="BTEC" className="BTEC-section">
-            <Box>
-            <h2 className="section-title">BTEC - City of Liverpool College</h2>
-            <Box className="BTEC-info">
-                <Box className="grid">
-                    <Box>
-                        <p>
-                            Currently studying BTEC Level 2 Information and Creative Technology at The City of Liverpool College.
-Learning web development, programming fundamentals, IT systems, digital technologies, and creative computing skills.
-Working with modern technologies and improving practical problem-solving abilities through projects and coursework.
-                        </p>
-                    </Box>
-                </Box>
-            <img src={img}/>
-            </Box>
-            </Box>
-        </section>
-    );
-}
+<section id="BTEC" className="BTEC-section">
+    <h2 className="section-title">BTEC - City of Liverpool College</h2>
 
-export default BTEC;
+    <Box className="BTEC-card">
+        <Box className="BTEC-text">
+            <h3>BTEC Level 2 ICT</h3>
+
+            <p>
+                Currently studying BTEC Level 2 Information and Creative Technology
+                at The City of Liverpool College.
+            </p>
+
+            <p>
+                Learning web development, programming fundamentals, IT systems,
+                digital technologies, and creative computing skills.
+            </p>
+
+            <p>
+                Improving practical problem-solving abilities through projects
+                and coursework.
+            </p>
+        </Box>
+
+        <Box className="BTEC-image-box">
+            <img src={img} alt="City of Liverpool College" />
+        </Box>
+    </Box>
+</section>
